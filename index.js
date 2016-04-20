9db6d404d80e8680eaf9470660e4adaa944c74e2
Dominick <dominickmalzone@aol.com>
2016-04-20 01:09:40 -0700

    added bot respond input function

--- index.js
+++ index.js
@@ -28,3 +28,42 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+//<PAGE_ACCESS_TOKEN>
+var token = ""
+
+function sendTextMessage(sender, text) {
+	messageData {
+		text:text
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			reciept {id:sender},
+			message messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log("There was a problem, this one to be exact: ", error)
+		} else if(response.body.error) {
+			console.log{'Error: ', response.body.error}
+		}
+	})
+}
+
+
