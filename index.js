7eb769ed7cf8bf2968fc28f5c9c1d69cb761db27
Dominick <dominickmalzone@aol.com>
2016-04-22 23:20:29 -0700

    updated personal msg

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post('/webhook/', function (req, res) {
 })
 
 //<PAGE_ACCESS_TOKEN>
-var token = "CAAY98J5R0d0BALGMyiFaQZAlnn3aZAu09pVfkN0fwO9JtvqyZCe5ZCYeGDvVcXFpBHaT0SZCfyHDqNmvCNOwmT2uSchRDFM6f87Vr8OFOFvs54BcZBG0XZA4w69UXZA52A52cjGGOfBVGWHJRHI64CaK8s46ZCc6lfmI1KDhwdvACGLZA5Yy7YRmWCNZCfTwjqciH4ZD"
+var token = "#"
 
 function sendTextMessage(sender, text) {
     messageData = {
@@ -85,8 +85,8 @@ function sendDomMessage(sender) {
                         "title": "His Site"
                     }, {
                         "type": "web_url",
-                        "url": "http://www.Dominickmalzone.com/fb",
-                        "title": "Resume for FB"
+                        "url": "#",
+                        "title": "Resume #"
                     }],
                 }, {
                     "title": "What does he build",
